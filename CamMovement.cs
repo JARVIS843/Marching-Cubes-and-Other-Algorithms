bf8a4398dd0cb5a402d7467f6df5c1c166cb49d3
Jarvis H Yang <jarvis.h.yang@outlook.com>
2021-06-03 01:14:19 +0800

    First Commit

---
+++ CamMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    public Camera cam;
+    // Update is called once per frame
+    void Update()
+    {
+          transform.position = Vector3.MoveTowards(transform.position, transform.position + (cam.transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal")), Time.deltaTime * 10f);
+        cam.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0 ));
+        cam.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 1f));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.tag == "Terrain")
+                    hit.transform.GetComponent<mc>().PlaceTerrain(hit.point);
+            }
+
+        }
+
+       if (Input.GetMouseButtonDown(1)) {
+           Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 1f));
+           RaycastHit hit;
+           if (Physics.Raycast(ray, out hit)) {
+               if (hit.transform.tag == "Terrain")
+                   hit.transform.GetComponent<mc>().RemoveTerrain(hit.point);
+               
+           }
+       }
+    }
+
+   
+}
